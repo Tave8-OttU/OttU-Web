767648d338cdc7571aaad67b5f42f03a93ca9742
runru1030 <runru1030@gmail.com>
2021-12-21 23:32:59 +0900

    [DESIGN]: global style setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import GlobalStyle from './assets/styles/global-style';
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
