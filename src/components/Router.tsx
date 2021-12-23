b15df690b174e5ff931098e44694bdd588fdf9ee
runru1030 <runru1030@gmail.com>
2021-12-23 13:47:54 +0900

    [FEAT]: posting router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddOtt from '../pages/AddOtt';
+import AddPost from '../pages/AddPost';
 import Login from '../pages/Login';
 import Main from '../pages/Main';
 import MyOtt from '../pages/MyOtt';
@@ -20,6 +21,7 @@ const AppRouter: React.FC = () => {
           <Route path=":ott" element={<Recruit />} />
         </Route>
         <Route path="/addott" element={<AddOtt />} />
+        <Route path="/posting" element={<AddPost />} />
       </Routes>
     </Router>
   );
