75349a02a2df8a660e0b6128839f8580a296a264
runru1030 <runru1030@gmail.com>
2022-01-03 10:56:16 +0900

    [FEAT]: ott's price object

--- src/assets/Objects/otts.tsx
+++ src/assets/Objects/otts.tsx
@@ -1,1 +1,24 @@
 export const Otts = ['netflix', 'tving', 'wavve', 'watcha', 'disney', 'cp'];
+export const Prices = [
+  [
+    { price: 9500, headCount: 1, title: '베이직' },
+    { price: 13500, headCount: 2, title: '스탠다드' },
+    { price: 17000, headCount: 4, title: '프리미엄' },
+  ],
+  [
+    { price: 7900, headCount: 1, title: '베이직' },
+    { price: 10900, headCount: 2, title: '스탠다드' },
+    { price: 13900, headCount: 4, title: '프리미엄' },
+  ],
+  [
+    { price: 7900, headCount: 1, title: '베이직' },
+    { price: 10900, headCount: 2, title: '스탠다드' },
+    { price: 13900, headCount: 4, title: '프리미엄' },
+  ],
+  [
+    { price: 7900, headCount: 1, title: '베이직' },
+    { price: 12900, headCount: 4, title: '프리미엄' },
+  ],
+  [{ price: 9900, headCount: 4, title: '디즈니 플러스' }],
+  [{ price: 2900, headCount: 2, title: '쿠팡 플레이' }],
+];
