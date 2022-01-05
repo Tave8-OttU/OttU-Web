6f6bc0615756b47312ecb5bb1df6b2aec6fa4d44
runru1030 <runru1030@gmail.com>
2022-01-05 14:06:19 +0900

    [FEAT]: 나의 ott만 필터링

--- src/components/Myott/OttContainer.tsx
+++ src/components/Myott/OttContainer.tsx
@@ -1,54 +1,60 @@
 import * as React from 'react';
 import { useNavigate } from 'react-router';
 import styled from 'styled-components';
-import { Otts } from '../../assets/Objects/otts';
+import { ott } from '../../pages/MyOtt';
 import OttImg from '../common/OttImg';
 interface props {
-  ott?: string;
+	ott?: string;
+	ottList: ott[];
 }
-const OttContainer: React.FC<props> = ({ ott }) => {
-  const navigate = useNavigate();
-  const onClickHandler = (event: React.MouseEvent) => {
-    const {
-      currentTarget: { id },
-    } = event;
-    navigate(`/myott/${id}`);
-  };
-  return (
-    <Wrapper>
-      <OttBox id={ott}>
-        <OttImg ott={ott ? ott : ''} width="200px" />
-      </OttBox>
-      {Otts.map(
-        (it) =>
-          ott != it && (
-            <OttBox id={it} isSmall onClick={onClickHandler}>
-              <OttImg ott={it} width="150px" />
-            </OttBox>
-          ),
-      )}
-    </Wrapper>
-  );
+const OttContainer: React.FC<props> = ({ ott, ottList }) => {
+	const navigate = useNavigate();
+
+	const onClickHandler = (event: React.MouseEvent) => {
+		const {
+			currentTarget: { id },
+		} = event;
+		navigate(`/myott/${id}`);
+	};
+	return (
+		<Wrapper>
+			<OttBox id={ott}>
+				<OttImg ott={ott ? ott : ''} width="200px" />
+			</OttBox>
+			{ottList.map(
+				(it) =>
+					ott != it.platform.platformName && (
+						<OttBox
+							id={it.platform.platformName}
+							isSmall
+							onClick={onClickHandler}
+						>
+							<OttImg ott={it.platform.platformName} width="150px" />
+						</OttBox>
+					)
+			)}
+		</Wrapper>
+	);
 };
 export default OttContainer;
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
 `;
 const OttBox = styled.div<{ isSmall?: boolean }>`
-  background-color: white;
-  width: 350px;
-  height: 185px;
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 20px 20px 20px 10px #00000025;
-  cursor: pointer;
-  ${(props) =>
-    props.isSmall &&
-    `
+	background-color: white;
+	width: 350px;
+	height: 185px;
+	border-radius: 10px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	box-shadow: 20px 20px 20px 10px #00000025;
+	cursor: pointer;
+	${(props) =>
+		props.isSmall &&
+		`
   width: 250px;
   height: 135px;
   opacity:50%;
