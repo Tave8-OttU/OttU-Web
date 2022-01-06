c6a8c66af7b2837e5cd321f0a81387902bc6da00
runru1030 <runru1030@gmail.com>
2022-01-06 14:00:30 +0900

    [FEAT]: 모집글 redux 적용

--- src/components/MyPost/PostContainer.tsx
+++ src/components/MyPost/PostContainer.tsx
@@ -1,23 +1,33 @@
 import axios from 'axios';
 import * as React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { RootState } from '../../modules';
+import { recuritObj, setPostList } from '../../modules/recruitList';
 import { recruitPost } from '../Recruit/Content';
 import Post from '../Recruit/Post/Post';
 const PostContainer: React.FC = () => {
+	const dispatch = useDispatch();
 	const { userObj } = useSelector((state: RootState) => state.user);
-	const [postList, setPostList] = React.useState<recruitPost[]>([]);
+	const { postList } = useSelector((state: RootState) => state.recruitList);
 	React.useEffect(() => {
 		axios.get(`/user/${userObj.userIdx}/recruit`).then((res) => {
-			res.status === 200 && setPostList(res.data.recruitlist);
+			res.status === 200 &&
+				dispatch(
+					setPostList(
+						res.data.recruitlist.map((post: recruitPost) => ({
+							postObj: { ...post },
+							isWriter: post.writer.userIdx === userObj.userIdx,
+						}))
+					)
+				);
 		});
 	}, []);
 
 	return (
 		<Container className="col-container">
-			{postList.map((post) => (
-				<Post postObj={post} isWriter={true} isMine={true} />
+			{postList.map((post: recuritObj, idx: number) => (
+				<Post idx={idx} isMine={true} />
 			))}
 		</Container>
 	);
