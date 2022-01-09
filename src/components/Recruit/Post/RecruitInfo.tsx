03f960cd591f23e0abe2f8d71856b29e03fc53af
runru1030 <runru1030@gmail.com>
2022-01-09 13:42:34 +0900

    [DESING/RECRIT]: 모집완료 라벨 수정

--- src/components/Recruit/Post/RecruitInfo.tsx
+++ src/components/Recruit/Post/RecruitInfo.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import Count from '../../common/Count';
-import { BlueLabel } from '../../common/Labels';
+import { BlueLabel, DarkGrayLabel, GrayLabel } from '../../common/Labels';
 import { recruitPost } from '../Content';
 
 interface props {
-  postObj: recruitPost;
+	postObj: recruitPost;
 }
 const RecruitInfo: React.FC<props> = ({ postObj }) => {
-  return (
-    <Container className="row-container">
-      <BlueLabel>{postObj.isCompleted ? '모집 완료' : '모집 중'}</BlueLabel>
-      <Count choiceNum={postObj.choiceNum} headcount={postObj.headcount} />
-    </Container>
-  );
+	return (
+		<Container className="row-container">
+			{postObj.isCompleted ? (
+				<DarkGrayLabel>모집 완료</DarkGrayLabel>
+			) : (
+				<BlueLabel>모집 중</BlueLabel>
+			)}
+			<Count choiceNum={postObj.choiceNum} headcount={postObj.headcount} />
+		</Container>
+	);
 };
 export default RecruitInfo;
 const Container = styled.div`
-  gap: 10px;
+	gap: 10px;
 `;
--- src/components/common/Labels.tsx
+++ src/components/common/Labels.tsx
@@ -1,19 +1,25 @@
 import styled from 'styled-components';
 
 export const GrayLabel = styled.span`
-  background-color: #535353;
-  border-radius: 50px;
-  padding: 10px 20px;
+	background-color: #535353;
+	border-radius: 50px;
+	padding: 10px 20px;
+`;
+export const DarkGrayLabel = styled.span`
+	background-color: #ffffff10;
+	color: #c4c4c4;
+	border-radius: 5px;
+	padding: 10px 20px;
 `;
 export const BorderGrayLabel = styled.span`
-  background-color: #00000075;
-  border: solid thin #45c7ff;
-  border-radius: 50px;
-  padding: 10px 20px;
+	background-color: #00000075;
+	border: solid thin #45c7ff;
+	border-radius: 50px;
+	padding: 10px 20px;
 `;
 export const BlueLabel = styled.span`
-  background-color: #45c7ff;
-  color: white;
-  border-radius: 5px;
-  padding: 10px 20px;
+	background-color: #45c7ff;
+	color: white;
+	border-radius: 5px;
+	padding: 10px 20px;
 `;
