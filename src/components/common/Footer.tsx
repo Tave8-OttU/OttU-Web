be7c16352da9b4a96b7bfe1dce11166a49efc71c
runru1030 <runru1030@gmail.com>
2022-01-09 13:55:22 +0900

    [FEAT/FOOTER]

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import * as React from 'react';
+import styled from 'styled-components';
+import logo from '../../assets/images/logo_s.png';
+const Footer: React.FC = () => {
+	return (
+		<Container>
+			<span>2022 &copy; OttU</span>
+			<img src={logo} width="80px" />
+		</Container>
+	);
+};
+export default Footer;
+const Container = styled.footer`
+	display: flex;
+	align-items: flex-end;
+	justify-content: space-between;
+	opacity: 50%;
+	padding: 20px 30px;
+	padding-top: 10px;
+	font-weight: lighter;
+	background-color: #000000;
+	margin-top: 50px;
+`;
